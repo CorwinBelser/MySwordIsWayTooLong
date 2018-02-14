c6e37687b67edb186de90206e67e8cef07ed6e5f
no-na <noahknam@gmail.com>
2018-02-14 16:26:24 -0500

    -Added test scene and scripts.

---
+++ Assets/Explode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject spark;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.GetComponent<Rigidbody>()){
+			other.gameObject.GetComponent<Rigidbody>().AddExplosionForce(1000f,other.gameObject.transform.position, 10f, 1f);
+			GameObject boom = Instantiate(explosion,transform.position,Quaternion.identity) as GameObject;
+			Destroy(boom, 1);
+		}
+		else{
+			GameObject boom = Instantiate(spark,transform.position,Quaternion.identity) as GameObject;
+			Destroy(boom, 1);
+		}
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody _rb;
+	public float turnRate;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		_rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * turnRate * Time.deltaTime);
+			transform.Rotate(Vector3.right * Input.GetAxis("Mouse Y") * turnRate * Time.deltaTime);
+			transform.Translate(Vector3.right * Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime);
+			transform.Translate(Vector3.forward * Input.GetAxisRaw("Vertical") * speed * Time.deltaTime);
+	}
+	
+}
