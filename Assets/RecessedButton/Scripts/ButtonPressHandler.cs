c995e91d51a9a6ec1e586cec9d14cd6f3eee061f
CorwinBelser <cmb539@drexel.edu>
2018-02-20 17:51:58 -0500

    Added recessed button asset. The button child object contains the script that invokes a UnityEvent when the Tip collider touches it

---
+++ Assets/RecessedButton/Scripts/ButtonPressHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonPressHandler : MonoBehaviour {
+
+    public UnityEvent ACTION;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Tip" && ACTION != null)
+            ACTION.Invoke();
+    }
+}
