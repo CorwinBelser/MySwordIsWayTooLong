45f660f9b3f469fe0b2ef1c2d39ebb59875f7af0
CorwinBelser <cmb539@drexel.edu>
2018-02-20 20:12:32 -0500

    Created level to aquire the sword, and ideally have to walk out backwards. Created prefabs for the player and sword with what I think are the current versions.

---
+++ Assets/Pedestal/Script/GivePlayerSword.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GivePlayerSword : MonoBehaviour {
+
+	public GameObject SWORD_IN_STONE;
+	public GameObject SWORD_ON_PLAYER;
+
+	private bool _actionCompleted = false;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player" && !_actionCompleted)
+		{
+			_actionCompleted = true;
+			/* Hide the sword in the stone */
+			SWORD_IN_STONE.SetActive(false);
+			/* Give the sword to the player */
+			SWORD_ON_PLAYER.SetActive(true);
+		}
+	}
+}
